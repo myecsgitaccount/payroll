f12f09b247e62baa2b7df27054142323fefad237
onyekachila <onyekachila@gmail.com>
2019-11-10 13:12:05 +0100

    Add employee controller

---
+++ PayRoll.Web/Models/EmployeeDeleteViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PayRoll.Web.Models
+{
+    public class EmployeeDeleteViewModel
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+    }
+}
